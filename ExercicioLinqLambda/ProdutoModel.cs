8086c9e6fc05ddb97fcd40d0c25f4e67c6a3026f
Daniel Haag <danni.haag91@gmail.com>
2021-12-10 22:40:48 -0300

    primeiro commit

---
+++ ExercicioLinqLambda/ProdutoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioLinqLambda
+{
+    class ProdutoModel
+    {
+        public string Nome { get; set; }
+
+        public double Valor { get; set; }
+    }
+}
---
+++ ExercicioLinqLambda/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+
+namespace ExercicioLinqLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pressione Enter para o arquivo .txt ser lido.");
+            Console.Read();
+
+            string path = "../../../Produtos.txt";
+
+            string[] linhas = File.ReadAllLines(path);
+
+            List<ProdutoModel> produtos = new List<ProdutoModel>();
+
+            for (int i = 0; i < linhas.Length; i++)
+            {
+                var produtoNomeValor = linhas[i].Trim().Split(',');
+
+                ProdutoModel produto = new ProdutoModel();
+
+                produto.Nome = produtoNomeValor[0];
+                produto.Valor = double.Parse(produtoNomeValor[1], CultureInfo.InvariantCulture);
+
+                produtos.Add(produto);
+            }
+
+            double valorMedio = double.Parse(produtos.Select(x => x.Valor).Average().ToString("F2"));
+
+            Console.WriteLine($"Valor médio: {valorMedio}");
+            Console.WriteLine($"Produtos ordenados:");
+
+            produtos = produtos.Where(x => x.Valor < valorMedio).OrderByDescending(x => x.Nome).ToList();
+
+            foreach (var item in produtos)
+            {
+                Console.WriteLine(item.Nome);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
